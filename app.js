590d664918c617053b05cb1248491ccfd604912d
s-arnus <28957746+s-arnus@users.noreply.github.com>
2018-09-29 16:39:07 +0300

    Added name calculator

--- app.js
+++ app.js
@@ -3,14 +3,32 @@
 'use strict';
 
 var myFirstApp = angular.module('myFirstApp', [])
-    //Used for some tests
     myFirstApp.controller('myFirstController', function ($scope) {
+        //Used for some tests
         $scope.firstname = "Juhan";
         $scope.surname = "";
         $scope.email = "test@gmail.com";
         $scope.function1 = function(){
             return("Return from function1")
         };
+        
+        //Name Calculator
+        $scope.name = "";
+        $scope.totalvalue = 0;
+        $scope.displayNumberic = function () {
+            var totalNameValue = calculateNumericForString($scope.name);
+            //Update scope variable to local local variable
+            $scope.totalvalue = totalNameValue;
+        };
+
+        function calculateNumericForString(string) {
+            var totalStringValue = 0;
+            for (var i = 0; i < string.length; i++) {
+                totalStringValue += string.charCodeAt(i);
+            }
+            return totalStringValue;
+        };
+
     });
 
 })();
